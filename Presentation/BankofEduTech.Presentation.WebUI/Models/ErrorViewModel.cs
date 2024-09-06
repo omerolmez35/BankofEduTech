b9768f474f64cc8a8f1da2d8c63ae15c45d309d5
Ömer ÖLMEZ <omrolmz@hotmail.com>
2024-09-06 17:37:12 +0300

    Add project files.

---
+++ Presentation/BankofEduTech.Presentation.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankofEduTech.Presentation.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
